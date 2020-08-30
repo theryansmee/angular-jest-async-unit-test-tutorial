9f28449f1baf24d3adb1756262750e6fe0ca11b3
Ryan Smee <theryansmee@gmail.com>
2020-08-30 13:22:41 +0100

    Added promises to test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationComponent } from './notification/notification.component';
+import { NameCheckerComponent } from './name-checker/name-checker.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		NotificationComponent
+		NotificationComponent,
+		NameCheckerComponent
 	],
 	imports: [
 		BrowserModule,
