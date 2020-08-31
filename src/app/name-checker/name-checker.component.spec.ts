859791aff8c5e2c2e6a889f66ecf8b6ef3992a41
Ryan Smee <theryansmee@gmail.com>
2020-08-31 16:14:45 +0100

    Mocked components in spec files

--- src/app/name-checker/name-checker.component.spec.ts
+++ src/app/name-checker/name-checker.component.spec.ts
@@ -1,3 +1,11 @@
-describe('NameCheckerComponent', () => {
+import { NameCheckerComponent } from './name-checker.component';
+
+
+describe( 'NameCheckerComponent', () => {
+	let fixture: NameCheckerComponent;
+
+	beforeEach(() => {
+		fixture = new NameCheckerComponent();
+	});
 
 });
--- src/app/notification/service/notification.service.spec.ts
+++ src/app/notification/service/notification.service.spec.ts
@@ -1,4 +1,13 @@
+import { NotificationService } from './notification.service';
 
 describe( 'NotificationService', () => {
+	let fixture: NotificationService;
+	let httpMock;
+
+	beforeEach( () => {
+		fixture = new NotificationService(
+			httpMock
+		);
+	});
 
 });
