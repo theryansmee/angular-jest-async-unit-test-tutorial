23d0410f739749f92bd8870ecd192bbc9415e8b5
Ryan Smee <theryansmee@gmail.com>
2020-09-01 11:36:07 +0100

    Mocked components in spec files

--- src/app/notification/notification.component.spec.ts
+++ src/app/notification/notification.component.spec.ts
@@ -1,3 +1,17 @@
-describe('NotificationComponent', () => {
+import { NotificationComponent } from './notification.component';
 
+
+describe('NotificationComponent', () => {
+	let fixture: NotificationComponent;
+	let notificationServiceMock;
+
+	beforeEach( () => {
+		notificationServiceMock = {
+			addNotificationToMarkAsReadPool: jest.fn()
+		};
+
+		fixture = new NotificationComponent(
+			notificationServiceMock
+		);
+	});
 });
