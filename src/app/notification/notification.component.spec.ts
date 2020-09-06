e820b1cfa1006da42315c8052daeb66e7ebb2dc7
Ryan Smee <theryansmee@gmail.com>
2020-09-06 18:49:08 +0100

    Added tests for Timers tutorial

--- src/app/notification/notification.component.spec.ts
+++ src/app/notification/notification.component.spec.ts
@@ -1,4 +1,5 @@
 import { NotificationComponent } from './notification.component';
+import { NotificationModel } from './notification.model';
 
 
 describe('NotificationComponent', () => {
@@ -13,5 +14,73 @@ describe('NotificationComponent', () => {
 		fixture = new NotificationComponent(
 			notificationServiceMock
 		);
+	});
+
+	describe( 'startMarkAsReadTimer', () => {
+		let notification: NotificationModel;
+
+		beforeEach(() => {
+			notification = {
+				id: 'n1',
+				title: 'You have a DM'
+			} as NotificationModel;
+			jest.useFakeTimers();
+		});
+
+		afterEach( () => {
+			jest.clearAllTimers();
+		});
+
+		it( 'should map the setTimout to markAsReadTimer', () => {
+			fixture.markAsReadTimer = undefined;
+
+			fixture.startMarkAsReadTimer( notification );
+
+			expect( fixture.markAsReadTimer ).toEqual( expect.any( Number ) );
+		});
+
+		it( 'should trigger addNotificationToMarkAsReadPool when the setTimeout fires', () => {
+
+			fixture.startMarkAsReadTimer( notification );
+			jest.runOnlyPendingTimers();
+
+			expect( notificationServiceMock.addNotificationToMarkAsReadPool ).toBeCalledWith( notification );
+		});
+
+		describe( 'markAsReadTimer boundaries', () => {
+			it( 'should NOT call addNotificationToMarkAsReadPool before 3seconds', () => {
+
+				fixture.startMarkAsReadTimer( notification );
+				jest.advanceTimersByTime( 2999 );
+
+				expect( notificationServiceMock.addNotificationToMarkAsReadPool ).not.toBeCalled();
+			});
+
+			it( 'should call addNotificationToMarkAsReadPool after 3seconds', () => {
+
+				fixture.startMarkAsReadTimer( notification );
+				jest.advanceTimersByTime( 3001 );
+
+				expect( notificationServiceMock.addNotificationToMarkAsReadPool ).toBeCalledTimes( 1 );
+			});
+
+			it( 'should NOT call addNotificationToMarkAsReadPool again after its been called', () => {
+
+				fixture.startMarkAsReadTimer( notification );
+				jest.advanceTimersByTime( 12000 );
+
+				expect( notificationServiceMock.addNotificationToMarkAsReadPool ).toBeCalledTimes( 1 );
+			});
+		});
+	});
+
+	describe( 'stopMarkAsReadTimer', () => {
+		it( 'should clear markAsReadTimer if it has been set', () => {
+			fixture.markAsReadTimer = setTimeout( () => {}, 2000 );
+
+			fixture.stopMarkAsReadTimer();
+
+			expect(fixture.markAsReadTimer ).toBeUndefined();
+		});
 	});
 });
--- src/app/notification/service/notification.service.spec.ts
+++ src/app/notification/service/notification.service.spec.ts
@@ -1,5 +1,6 @@
 import { NotificationService } from './notification.service';
 
+
 describe( 'NotificationService', () => {
 	let fixture: NotificationService;
 	let httpMock;
@@ -10,4 +11,58 @@ describe( 'NotificationService', () => {
 		);
 	});
 
+	describe( 'startMarkAsReadInterval', () => {
+		let markNotificationsAsReadSpy;
+
+		beforeEach( () => {
+			jest.useFakeTimers();
+			markNotificationsAsReadSpy = jest.spyOn( fixture, 'markNotificationsAsRead' );
+		});
+
+		afterEach( () => {
+			jest.clearAllTimers();
+		});
+
+		it( 'should map setInterval to markAsReadInterval', () => {
+			fixture.markAsReadInterval = undefined;
+
+			fixture.startMarkAsReadInterval();
+
+			expect( fixture.markAsReadInterval ).toEqual( expect.any( Number ) );
+		});
+
+		it( 'should trigger markNotificationsAsRead when the setInterval fires', () => {
+
+			fixture.startMarkAsReadInterval();
+			jest.runOnlyPendingTimers();
+
+			expect( markNotificationsAsReadSpy ).toBeCalled();
+		});
+
+		describe( 'markAsReadInterval boundaries', () => {
+			it( 'should NOT trigger markNotificationsAsRead before 5seconds', () => {
+
+				fixture.startMarkAsReadInterval();
+				jest.advanceTimersByTime( 4999 );
+
+				expect( markNotificationsAsReadSpy ).not.toBeCalled();
+			});
+
+			it( 'should trigger markNotificationsAsRead after 5seconds', () => {
+
+				fixture.startMarkAsReadInterval();
+				jest.advanceTimersByTime( 5001 );
+
+				expect( markNotificationsAsReadSpy ).toBeCalledTimes( 1 );
+			});
+
+			it( 'should trigger markNotificationsAsRead 4 times in 20seconds', () => {
+
+				fixture.startMarkAsReadInterval();
+				jest.advanceTimersByTime( 20001 );
+
+				expect( markNotificationsAsReadSpy ).toBeCalledTimes( 4 );
+			});
+		});
+	});
 });
